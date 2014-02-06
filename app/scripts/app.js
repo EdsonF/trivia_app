0cd26fb1a3a2ba0a3de9e9b72ad53502e7fc0a94
Michael Mandato <michaelmandato@dreamfactory.com>
2014-02-06 10:18:14 -0500

    Add DreamFactory AngularJS Module.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,19 @@
 'use strict';
 
-angular.module('triviaappApp', [
-  'ngCookies',
-  'ngSanitize'
-]);
+angular.module('triviaApp', [
+        'ngRoute',
+        'ngResource',
+        'ngDreamFactory'
+    ])
+    .constant('DSP_URL', 'https://dsp-movie.cloud.dreamfactory.com')
+    .constant('DSP_API_KEY', 'launchpad')
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,9 @@
 'use strict';
 
-angular.module('triviaappApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('triviaApp')
+  .controller('MainCtrl', ['$scope', 'DreamFactory', function ($scope, DreamFactory) {
+
+
+
+
+  }]);
